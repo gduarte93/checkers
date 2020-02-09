0ef2fc07582f4f0182b3fa1e9e47ff71a5df6f17
Gabriel Duarte <gduarte93@aol.com>
2020-02-09 13:48:33 -0500

    only use build files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-var React    = require('react'),
-    ReactDOM = require('react-dom'),
-    App      = require('./components/App/index.jsx');
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
